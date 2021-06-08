75909bab452e1959cc519c1cbd522adcecae0470
Eva Afriana <vava.afriana@gmail.com>
2021-06-08 11:18:13 +0200

    add validation or error messages to radio and checkbox

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,107 +13,131 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const modalClose = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
 
+// launch modal form
+const launchModal = () => {
+  modalbg.style.display = "block";
+};
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
+// close modal form
+const closeModal = () => {
+  modalbg.style.display = "none";
+};
 
 // close modal event
 modalClose.forEach((btn) => btn.addEventListener("click", closeModal));
 
-// close modal form
-function closeModal() {
-  modalbg.style.display = "none";
-}
+// is a radio btn checked ?
+const isRadioChecked = (arr) => {
+  let res = false;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].type === "radio" && arr[i].checked) {
+      res = true;
+    }
+  }
+  return res;
+};
 
 // form validation
-function validate() {
+const validate = () => {
   let first = document.getElementById("first").value;
   let last = document.getElementById("last").value;
   let email = document.getElementById("email").value;
   let birthdate = document.getElementById("birthdate").value;
   let quantity = document.getElementById("quantity").value;
-  // let radios = document.getElementsByName("location");
-  // let checkBox = document.getElementById("checkbox1").checked;
+  let radios = document.getElementsByName("location");
+  let checkBox = document.getElementById("checkbox1").checked;
 
-  let valid = 1;
+  console.log(radios);
 
-  let namereg = /^[a-zA-Z]+$/;
+  let valid = true;
+
+  let namereg = /^[a-zA-Z][a-zA-Z- ]+[a-zA-Z]$/;
   let emailreg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+[a-zA-Z]{2,}$/;
 
-  if (first.trim() == "" && valid == 1) {
-    document.getElementById("label-first").style.display = "inline";
-    document.getElementById("label-first").innerHTML =
-      "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
-    valid = 0;
-  }
-  if (!first.match(namereg) && valid == 1) {
+  // reset error messages
+  [
+    "label-first",
+    "label-last",
+    "label-email",
+    "label-birthdate",
+    "label-quantity",
+    "messsage-checked",
+    "messsage-checkbox1",
+  ].forEach((element) => {
+    if (document.getElementById(element)) {
+      document.getElementById(element).style.display = "none";
+    }
+  });
+
+  // check errors
+  if (!first.match(namereg)) {
     document.getElementById("label-first").style.display = "inline";
     document.getElementById("label-first").innerHTML =
       "Le prénom n'est pas valide";
-    valid = 0;
+    valid = false;
+  }
+  if (first.trim() === "") {
+    document.getElementById("label-first").style.display = "inline";
+    document.getElementById("label-first").innerHTML =
+      "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+    valid = false;
   }
 
-  if (last.trim() == "" && valid == 1) {
+  if (!last.match(namereg)) {
+    document.getElementById("label-last").style.display = "inline";
+    document.getElementById("label-last").innerHTML = "Le nom n'est pas valide";
+    valid = false;
+  }
+  if (last.trim() === "") {
     document.getElementById("label-last").style.display = "inline";
     document.getElementById("label-last").innerHTML =
       "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
-    valid = 0;
-  }
-  if (!last.match(namereg) && valid == 1) {
-    document.getElementById("label-last").style.display = "inline";
-    document.getElementById("label-last").innerHTML = "Le nom n'est pas valide";
-    valid = 0;
+    valid = false;
   }
 
-  if (email.trim() == "" && valid == 1) {
+  if (!email.match(emailreg)) {
     document.getElementById("label-email").style.display = "inline";
     document.getElementById("label-email").innerHTML =
-      "Please enter your email";
-    valid = 0;
+      "Veuillez entrer un email valide.";
+    valid = false;
   }
-  if (!email.match(emailreg) && valid == 1) {
+  if (email.trim() === "") {
     document.getElementById("label-email").style.display = "inline";
-    document.getElementById("label-email").innerHTML = "Invalid email";
-    valid = 0;
+    document.getElementById("label-email").innerHTML =
+      "Veuillez entrer un email.";
+    valid = false;
   }
 
-  if (birthdate.trim() == "" && valid == 1) {
+  if (birthdate.trim() === "") {
     document.getElementById("label-birthdate").style.display = "inline";
     document.getElementById("label-birthdate").innerHTML =
-      "Vous devez entrer votre date de naissance.";
-    valid = 0;
+      "Veuillez entrer votre date de naissance.";
+    valid = false;
   }
 
-  if (quantity.trim() == "" && valid == 1) {
+  if (quantity.trim() === "") {
     document.getElementById("label-quantity").style.display = "inline";
     document.getElementById("label-quantity").innerHTML =
-      "Vous devez entrer le numero";
-    valid = 0;
+      "Veuillez entrer un numero.";
+    valid = false;
   }
 
-  // for (let i = 0; i < radios.length; i++) {
-  //   if (radios[i].type == "radio" && radios[i].checked && valid == 1) {
-  //     document.getElementById("label-location").style.display = "inline";
-  //     document.getElementById("label-location").innerHTML =
-  //       "Vous devez choisir une option.";
-  //     valid = 0;
-  //   }
-  // }
-
-  // if (checkBox.checked == false && valid == 1) {
-  //   document.getElementById("label-checkbox1").style.display = "inline";
-  //   document.getElementById("label-checkbox1").innerHTML =
-  //     "Vous devez vérifier que vous acceptez les termes et conditions.";
-  //   valid = 0;
-  // }
-
-  if (valid == 1) {
-    return true;
-  } else {
-    return false;
+  if (!isRadioChecked(radios)) {
+    document.getElementById("messsage-checked").style.display = "inline";
+    document.getElementById("messsage-checked").innerHTML =
+      "Veuillez choisir une option.";
+    valid = false;
   }
-}
+
+  if (!checkBox) {
+    document.getElementById("messsage-checkbox1").style.display = "inline";
+    document.getElementById("messsage-checkbox1").innerHTML =
+      "Vous devez vérifier que vous acceptez les termes et conditions.";
+    valid = false;
+  }
+
+  return valid;
+};
